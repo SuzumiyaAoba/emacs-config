d6eca66f5af2c7e36e39c9d822f0210ca2f8bd4e
SuzumiyaAoba <SuzumiyaAoba@gmail.com>
2025-01-19 18:07:02 +0900

    refactor: build isolated Emacs environment in Nix store

--- emacs-init.nix
+++
@@ -1,7 +0,0 @@
-{ pkgs ? import <nixpkgs> {} }:
-
-pkgs.writeTextFile {
-  name = "emacs-init";
-  destination = "/share/emacs/site-lisp/default.el";
-  text = builtins.readFile ./init.el;
-}
--- flake.nix
+++ flake.nix
@@ -13,17 +13,51 @@
       let
         overlays = [ emacs-overlay.overlay ];
         pkgs = import nixpkgs { inherit system overlays; };
+
+        # Emacsパッケージを設定
+        emacsPackage = pkgs.emacsWithPackagesFromUsePackage {
+          package = pkgs.emacs-unstable;
+          config = ./config.org;
+          extraEmacsPackages = epkgs: with epkgs; [
+            nix-mode
+          ];
+        };
+
+        # 設定ファイルを tangle して .emacs.d を作成する derivation
+        emacsConfig = pkgs.stdenv.mkDerivation {
+          name = "emacs-config";
+          src = ./.;
+
+          buildInputs = [ pkgs.emacs-unstable ];
+
+          buildPhase = ''
+            # .emacs.dディレクトリを作成
+            mkdir -p $out/.emacs.d
+
+            # config.orgをtangle
+            emacs --batch \
+              --eval "(require 'org)" \
+              --eval "(org-babel-tangle-file \"config.org\")"
+
+            # 生成されたファイルを.emacs.dに移動
+            mv init.el $out/.emacs.d/
+            mv early-init.el $out/.emacs.d/
+          '';
+
+          dontInstall = true;
+          dontFixup = true;
+        };
+
+        # Emacsの起動スクリプト
+        wrappedEmacs = pkgs.writeScriptBin "emacs" ''
+          #!${pkgs.bash}/bin/bash
+          exec ${emacsPackage}/bin/emacs \
+            --init-directory="${emacsConfig}/.emacs.d" \
+            "$@"
+        '';
       in
         {
-          packages.default = (pkgs.emacsWithPackagesFromUsePackage {
-            package = pkgs.emacs-unstable;
-            config = ./config.org;
-
-            extraEmacsPackages = epkgs: with epkgs; [
-              nix-mode
-              (pkgs.callPackage ./emacs-init.nix {})
-            ];
-          });
+          packages.default = wrappedEmacs;
         }
     );
 }
