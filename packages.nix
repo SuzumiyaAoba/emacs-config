fe9e18b1b35f8255a3bbefa412197a6fe2466405
SuzumiyaAoba <SuzumiyaAoba@gmail.com>
2025-09-19 21:41:26 +0900

    fix: treesit-grammars

--- packages.nix
+++ packages.nix
@@ -97,7 +97,34 @@ epkgs: with epkgs; [
   treemacs-tab-bar
   treesit-auto
   treesit-fold
-  treesit-grammars
+  (treesit-grammars.with-grammars (
+    p: with p; [
+      tree-sitter-bash
+      tree-sitter-c
+      tree-sitter-c-sharp
+      tree-sitter-cmake
+      tree-sitter-css
+      tree-sitter-scss
+      tree-sitter-dockerfile
+      tree-sitter-elisp
+      tree-sitter-go
+      tree-sitter-html
+      tree-sitter-java
+      tree-sitter-javascript
+      tree-sitter-json
+      tree-sitter-make
+      tree-sitter-markdown
+      tree-sitter-markdown-inline
+      tree-sitter-nix
+      tree-sitter-python
+      tree-sitter-ruby
+      tree-sitter-rust
+      tree-sitter-toml
+      tree-sitter-tsx
+      tree-sitter-typescript
+      tree-sitter-yaml
+    ]
+  ))
   tsx-mode
   coverlay
   undohist
